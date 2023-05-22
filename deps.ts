21927fb8f82e2b432387b68246990c24622192da
stefangohlke <gohlke.stefan@yahoo.de>
2023-05-22 23:27:09 +0200

    Update dependencies

--- deps.ts
+++ deps.ts
@@ -2,13 +2,13 @@ export {
    JSON_CONTENT_TYPE_HEADER,
    returnDataResponse,
    startServer,
-} from 'https://raw.githubusercontent.com/sgohlke/deno-web/1.0.1/index.ts'
+} from 'https://raw.githubusercontent.com/sgohlke/deno-web/2.0.1/index.ts'
 export type {
    AggregateError,
    GraphQLRequestInfo,
    GraphQLServerRequest,
    GraphQLServerResponse,
    MetricsClient,
-} from 'npm:@dreamit/graphql-server@4.0.0'
-export { GraphQLServer, JsonLogger } from 'npm:@dreamit/graphql-server@4.0.0'
+} from 'npm:@dreamit/graphql-server@4.1.0'
+export { GraphQLServer, JsonLogger } from 'npm:@dreamit/graphql-server@4.1.0'
 export { buildSchema, GraphQLError, GraphQLSchema } from 'npm:graphql'
