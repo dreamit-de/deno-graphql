24428f2a2357cf76a58d05d97d637eadd6ee98f3
sgohlke <gohlke.stefan@yahoo.de>
2023-07-08 14:34:10 +0200

    Update deno-web

--- deps.ts
+++ deps.ts
@@ -1,14 +1,15 @@
 export {
-   JSON_CONTENT_TYPE_HEADER,
    returnDataResponse,
-   startServer,
-} from 'https://raw.githubusercontent.com/sgohlke/deno-web/3.0.0/mod.ts'
+} from 'https://raw.githubusercontent.com/sgohlke/deno-web/6.0.0/response/responseFunctions.ts'
+export { JSON_CONTENT_TYPE_HEADER } from 'https://raw.githubusercontent.com/sgohlke/deno-web/6.0.0/response/ResponseHeaders.ts'
+export { startServer } from 'https://raw.githubusercontent.com/sgohlke/deno-web/6.0.0/server/serverFunctions.ts'
+
 export type {
    AggregateError,
    GraphQLRequestInfo,
    GraphQLServerRequest,
    GraphQLServerResponse,
    MetricsClient,
-} from 'npm:@dreamit/graphql-server@4.1.0'
-export { GraphQLServer, JsonLogger } from 'npm:@dreamit/graphql-server@4.1.0'
+} from 'npm:@dreamit/graphql-server@4.2.4'
+export { GraphQLServer, JsonLogger } from 'npm:@dreamit/graphql-server@4.2.4'
 export { buildSchema, GraphQLError, GraphQLSchema } from 'npm:graphql'
