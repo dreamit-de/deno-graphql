a9034b0721f7f40b386a49f63d401ce61c74d776
sgohlke <gohlke.stefan@yahoo.de>
2023-08-21 22:04:33 +0200

    Update dependencies

--- deps.ts
+++ deps.ts
@@ -1,8 +1,8 @@
 export {
    returnDataResponse,
-} from 'https://raw.githubusercontent.com/sgohlke/deno-web/6.0.0/response/responseFunctions.ts'
-export { JSON_CONTENT_TYPE_HEADER } from 'https://raw.githubusercontent.com/sgohlke/deno-web/6.0.0/response/ResponseHeaders.ts'
-export { startServer } from 'https://raw.githubusercontent.com/sgohlke/deno-web/6.0.0/server/serverFunctions.ts'
+} from 'https://raw.githubusercontent.com/sgohlke/deno-web/6.0.1/response/responseFunctions.ts'
+export { JSON_CONTENT_TYPE_HEADER } from 'https://raw.githubusercontent.com/sgohlke/deno-web/6.0.1/response/ResponseHeaders.ts'
+export { startServer } from 'https://raw.githubusercontent.com/sgohlke/deno-web/6.0.1/server/serverFunctions.ts'
 
 export type {
    AggregateError,
@@ -10,6 +10,6 @@ export type {
    GraphQLServerRequest,
    GraphQLServerResponse,
    MetricsClient,
-} from 'npm:@dreamit/graphql-server@4.2.4'
-export { GraphQLServer, JsonLogger } from 'npm:@dreamit/graphql-server@4.2.4'
+} from 'npm:@dreamit/graphql-server@4.2.6'
+export { GraphQLServer, JsonLogger } from 'npm:@dreamit/graphql-server@4.2.6'
 export { buildSchema, GraphQLError, GraphQLSchema } from 'npm:graphql'
