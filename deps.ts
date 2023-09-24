c1433fd0f0c5e27d673e08be364d406bd2e31155
sgohlke <gohlke.stefan@yahoo.de>
2023-09-24 17:22:26 +0200

    Update dependencies

--- deps.ts
+++ deps.ts
@@ -1,11 +1,11 @@
 export {
    assertEquals,
-} from 'https://raw.githubusercontent.com/sgohlke/deno-web/6.0.1/deps.ts'
+} from 'https://raw.githubusercontent.com/sgohlke/deno-web/6.0.2/deps.ts'
 export {
    returnDataResponse,
-} from 'https://raw.githubusercontent.com/sgohlke/deno-web/6.0.1/response/responseFunctions.ts'
-export { JSON_CONTENT_TYPE_HEADER } from 'https://raw.githubusercontent.com/sgohlke/deno-web/6.0.1/response/ResponseHeaders.ts'
-export { startServer } from 'https://raw.githubusercontent.com/sgohlke/deno-web/6.0.1/server/serverFunctions.ts'
+} from 'https://raw.githubusercontent.com/sgohlke/deno-web/6.0.2/response/responseFunctions.ts'
+export { JSON_CONTENT_TYPE_HEADER } from 'https://raw.githubusercontent.com/sgohlke/deno-web/6.0.2/response/ResponseHeaders.ts'
+export { startServer } from 'https://raw.githubusercontent.com/sgohlke/deno-web/6.0.2/server/serverFunctions.ts'
 
 export type {
    AggregateError
