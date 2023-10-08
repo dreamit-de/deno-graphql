109fa2c6b0d4aa2ead641e87fd07095a211823fb
sgohlke <gohlke.stefan@yahoo.de>
2023-10-08 15:40:45 +0200

    Update dependencies

--- deps.ts
+++ deps.ts
@@ -1,16 +1,16 @@
 export {
    assertEquals,
-} from 'https://raw.githubusercontent.com/sgohlke/deno-web/6.0.2/deps.ts'
+} from 'https://raw.githubusercontent.com/sgohlke/deno-web/7.0.0/deps.ts'
 export {
    returnDataResponse,
-} from 'https://raw.githubusercontent.com/sgohlke/deno-web/6.0.2/response/responseFunctions.ts'
-export { JSON_CONTENT_TYPE_HEADER } from 'https://raw.githubusercontent.com/sgohlke/deno-web/6.0.2/response/ResponseHeaders.ts'
-export { startServer } from 'https://raw.githubusercontent.com/sgohlke/deno-web/6.0.2/server/serverFunctions.ts'
+} from 'https://raw.githubusercontent.com/sgohlke/deno-web/7.0.0/response/responseFunctions.ts'
+export { JSON_CONTENT_TYPE_HEADER } from 'https://raw.githubusercontent.com/sgohlke/deno-web/7.0.0/response/ResponseHeaders.ts'
+export { startServer } from 'https://raw.githubusercontent.com/sgohlke/deno-web/7.0.0/server/serverFunctions.ts'
 
 export type {
    AggregateError
-} from 'npm:@dreamit/graphql-server@4.2.6'
-export { GraphQLServer, JsonLogger } from 'npm:@dreamit/graphql-server@4.2.6'
+} from 'npm:@dreamit/graphql-server@4.2.8'
+export { GraphQLServer, JsonLogger } from 'npm:@dreamit/graphql-server@4.2.8'
 export type {
    GraphQLRequestInfo,
    GraphQLServerRequest,
